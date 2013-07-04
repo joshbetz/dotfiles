4ff12f0ad89e9a189043b86457e7aa8871bad01b
Josh Betz <j@joshbetz.com>
2013-07-03 21:58:10 -0500

    Introduce installer

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get current directory
+CUR = pwd
+
+# Move Shell files into place
+rm -rf .zshrc
+rm -rf .bashrc
+ln -s ~/bin/dotfiles/shellrc ~/.zshrc
+ln -s ~/bin/dotfiles/shellrc ~/.bashrc
+
+# Setup git
+rm -rf ~/.gitconfig
+rm -rf ~/.gitignore
+ln -s ~/bin/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/bin/dotfiles/gitignore ~/.gitignore
+
+# Move Sublime files into place
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3
+ln -s ~/bin/dotfiles/Sublime\ Text\ 3 ~/Library/Application\ Support/
