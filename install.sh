43f1b89150f7bdf8f5ad34de2dd5c274afc6ba6e
Josh Betz <j@joshbetz.com>
2013-07-19 15:22:00 +0000

    Bigger comment blocks
    The small comments above each section didn't make it super easy
    to see where new sections begin at a glance. With bigger headings
    and a bit more space above the header, it will be easier to parse
    quickly and find the section you're looking for

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 # Get current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
+# =====================================
 # Setup ZSH
+# =====================================
 if ! [ -h ~/.zshrc ]; then
 	[ -e ~/.zshrc.local ] && mv ~/.zshrc.local ~/.zshrc.local.bak
 	[ -e ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.local
@@ -12,7 +15,10 @@ else
 fi
 ln -s $DIR/shellrc ~/.zshrc
 
+
+# =====================================
 # Setup Bash
+# =====================================
 if ! [ -h ~/.bashrc ]; then
 	[ -e ~/.bashrc.local ] && mv ~/.bashrc.local ~/.bashrc.local.bak
 	[ -e ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.local
@@ -21,7 +27,10 @@ else
 fi
 ln -s $DIR/shellrc ~/.bashrc
 
+
+# =====================================
 # Setup VIM
+# =====================================
 if ! [ -h ~/.vimrc ]; then
 	[ -e ~/.vimrc.local ] && mv ~/.vimrc.local.bak
 	[ -e ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.local
@@ -33,24 +42,39 @@ ln -s $DIR/vimrc ~/.vimrc
 [ -e ~/.vim ] && mv ~/.vim ~/.vim.bak
 ln -s $DIR/vim ~/.vim
 
+
+# =====================================
 # Setup git
+# =====================================
 rm -rf ~/.gitconfig
 rm -rf ~/.gitignore
 ln -s $DIR/gitconfig ~/.gitconfig
 ln -s $DIR/gitignore ~/.gitignore
 
+
+# =====================================
 # Setup ack
+# =====================================
 rm -rf ~/.ackrc
 ln -s $DIR/ackrc ~/.ackrc
 
+
+# =====================================
 # Setup tmux
+# =====================================
 rm -rf ~/.tmux.conf
 ln -s $DIR/tmux ~/.tmux.conf
 
+
+# =====================================
 # Setup ruby
+# =====================================
 rm -rf ~/.gemrc
 ln -s $DIR/gemrc ~/.gemrc
 
-# Move Sublime files into place
+
+# =====================================
+# Setup Sublime
+# =====================================
 rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3
 ln -s $DIR/Sublime\ Text\ 3 ~/Library/Application\ Support/
