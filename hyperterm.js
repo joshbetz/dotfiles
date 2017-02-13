4931e41c1125e85b69e9090a306ee2747848b210
Josh Betz <j@joshbetz.com>
2017-02-13 12:27:28 -0600

    Install hyperterm-1password, hyper-tabs-enhanced, hyperlinks

--- hyperterm.js
+++ hyperterm.js
@@ -1,68 +1,55 @@
 module.exports = {
-  config: {
-    // default font size in pixels for all tabs
-    fontSize: 18,
+	config: {
+		// default font size in pixels for all tabs
+		fontSize: 18,
 
-    windowSize: [800, 500],
+		windowSize: [800, 500],
 
-    // font family with optional fallbacks
-    fontFamily: 'Inconsolata, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+		// font family with optional fallbacks
+		fontFamily: 'Inconsolata, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
-    // terminal cursor background color (hex)
-    cursorColor: '#F81CE5',
+		// some color overrides. see http://bit.ly/29k1iU2 for
+		// the full list
+		colors: [
+			'#000000',
+			'#ff0000',
+			'#33ff00',
+			'#ffff00',
+			'#0066ff',
+			'#cc00ff',
+			'#00ffff',
+			'#d0d0d0',
+			'#808080',
+			'#ff0000',
+			'#33ff00',
+			'#ffff00',
+			'#0066ff',
+			'#cc00ff',
+			'#00ffff',
+			'#ffffff'
+		],
 
-    // color of the text
-    foregroundColor: '#fff',
+		hyperlinks: {
+			clickAction: 'ignore',
+			defaultBrowser: true
+		}
+	},
 
-    // terminal background color
-    backgroundColor: '#000',
+	// a list of plugins to fetch and install from npm
+	// format: [@org/]project[#version]
+	// examples:
+	//   `hypersolar`
+	//   `@company/project`
+	//   `project#1.0.1`
+	plugins: [
+		'hyper-tomorrow-night',
+		'hyperlinks',
+		'hyperterm-1password',
+		'hyper-tabs-enhanced',
+	],
 
-    // border color (window, tabs)
-    borderColor: '#333',
-
-    // custom css to embed in the main window
-    css: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    termCSS: '',
-
-    // custom padding
-    padding: '12px 14px',
-
-    // some color overrides. see http://bit.ly/29k1iU2 for
-    // the full list
-    colors: [
-      '#000000',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#d0d0d0',
-      '#808080',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#ffffff'
-    ]
-  },
-
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hypersolar`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: [
-    'hyperterm-tomorrow-night'
-  ],
-
-  // in development, you can create a directory under
-  // `~/.hyperterm_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: []
+	// in development, you can create a directory under
+	// `~/.hyperterm_plugins/local/` and include it here
+	// to load it and avoid it being `npm install`ed
+	localPlugins: []
 };
