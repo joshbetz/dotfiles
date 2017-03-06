e68c7039ac7291a8090a8fb59be37d2aa440ded7
Josh Betz <j@joshbetz.com>
2017-03-06 11:35:55 -0600

    Simplify install script

--- install.sh
+++ install.sh
@@ -3,60 +3,20 @@
 # Get current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# =====================================
-# Installation Helpers
-# =====================================
-dotfiles_backup() {
-	FILE=$1
-	if ! [ -h ~/.$FILE ]; then
-		[ -e ~/.$FILE.local ] && mv ~/.$FILE.local ~/.$FILE.local.bak
-		[ -e ~/.$FILE ] && mv ~/.$FILE ~/.$FILE.local
-	fi
-}
+# Shell
+ln -sfnv $DIR/shellrc ~/.zshrc
+ln -sfnv $DIR/shellrc ~/.bashrc
 
-dotfiles_install() {
-	FILE=$1
-	dotfiles_backup $FILE
-	rm -rf ~/.$FILE
-	ln -s $DIR/$FILE ~/.$FILE
-}
+# Hyperterm
+ln -sfnv $DIR/hyperterm.js ~/.hyperterm.js
 
-
-# =====================================
-# Setup ZSH
-# =====================================
-dotfiles_backup zshrc
-rm -f ~/.zshrc
-ln -s $DIR/shellrc ~/.zshrc
-
-
-# =====================================
-# Setup Bash
-# =====================================
-dotfiles_backup bashrc
-rm -f ~/.bashrc
-ln -s $DIR/shellrc ~/.bashrc
-
-# =====================================
-# Setup Hyperterm
-# =====================================
-dotfiles_install hyperterm.js
-
-
-# =====================================
-# Setup VIM
-# =====================================
-dotfiles_install vimrc
-dotfiles_install gvimrc
-dotfiles_install vim
-
-# Run vundle installer
+# Vim
+ln -sfnv $DIR/vim ~/.vim
+ln -sfnv $DIR/vimrc ~/.vimrc
+ln -sfnv $DIR/gvimrc ~/.gvimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-
-# =====================================
-# Setup git
-# =====================================
-dotfiles_install gitconfig
-dotfiles_install gitignore
+# Git
+ln -sfnv $DIR/gitconfig ~/.gitconfig
+ln -sfnv $DIR/gitignore ~/.gitignore
